9172059474ec88f0d856e2b2baf59b107b0e49f9
hoejeong <hoejeong1@naver.com>
2025-06-24 17:57:44 +0900

    ㅓjoin 리셋

--- lib/ui/pages/auth/join_page/join_page.dart
+++ lib/ui/pages/auth/join_page/join_page.dart
@@ -1,4 +1,3 @@
-import 'package:ballkkaye_frontend/_core/style/m_color.dart';
 import 'package:flutter/material.dart';
 
 class JoinPage extends StatelessWidget {
@@ -6,51 +5,6 @@ class JoinPage extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        title: Text('회원가입'),
-        elevation: 0,
-      ),
-      body: SafeArea(
-        child: Column(
-          children: [
-            SizedBox(height: 16),
-            _ProgressIndicator(currentPage: 1),
-            Expanded(
-              child: Padding(
-                padding: EdgeInsets.all(24),
-              ),
-            ),
-          ],
-        ),
-      ),
-    );
-  }
-}
-
-class _ProgressIndicator extends StatelessWidget {
-  final int currentPage;
-
-  const _ProgressIndicator({required this.currentPage});
-
-  @override
-  Widget build(BuildContext context) {
-    return Row(
-      mainAxisAlignment: MainAxisAlignment.center,
-      children: List.generate(3, (index) {
-        bool isActive = index == currentPage;
-
-        return AnimatedContainer(
-          duration: Duration(milliseconds: 200),
-          margin: const EdgeInsets.symmetric(horizontal: 4),
-          width: isActive ? 12 : 8,
-          height: 8,
-          decoration: BoxDecoration(
-            color: isActive ? MColor.kPrimary.strong : MColor.kFill.normal,
-            borderRadius: BorderRadius.circular(4),
-          ),
-        );
-      }),
-    );
+    return const Placeholder();
   }
 }
