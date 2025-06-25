5d9935851ffd9013804af1961b6cff197195e6e6
son7571 <sssk1755@gmail.com>
2025-06-25 13:25:02 +0900

    appbar 메서드화 floatingActionButton 추가

---
+++ lib/ui/pages/holder/user_match/detail_page/user_match_detail_page.dart
@@ -0,0 +1,16 @@
+import 'package:flutter/material.dart';
+
+class UserMatchDetailPage extends StatelessWidget {
+  const UserMatchDetailPage({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Center(
+        child: Container(
+          child: Text("동행 상세보기 페이지"),
+        ),
+      ),
+    );
+  }
+}
