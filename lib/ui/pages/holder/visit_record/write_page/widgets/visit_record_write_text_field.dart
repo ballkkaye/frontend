7ae37586fd6cee25a537e7e6ea36c69af03d75b1
jh0804 <wngml0708@gmail.com>
2025-06-27 16:44:10 +0900

    날짜,경기 선택 제외 작성 페이지 구현 완료

---
+++ lib/ui/pages/holder/visit_record/write_page/widgets/visit_record_write_text_field.dart
@@ -0,0 +1,29 @@
+import 'package:ballkkaye_frontend/_core/style/m_color.dart';
+import 'package:flutter/material.dart';
+
+class VisitRecordWriteTextField extends StatelessWidget {
+  const VisitRecordWriteTextField({
+    super.key,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return TextField(
+      maxLines: 10,
+      maxLength: 1000,
+      decoration: InputDecoration(
+        hintText: "경기 기록을 자유롭게 적어주세요",
+        hintStyle: TextStyle(color: MColor.kLabel.disable),
+        counterText: "0 / 1000",
+        enabledBorder: OutlineInputBorder(
+          borderRadius: BorderRadius.circular(8),
+          borderSide: BorderSide(color: MColor.kLine.normal),
+        ),
+        focusedBorder: OutlineInputBorder(
+          borderRadius: BorderRadius.circular(8),
+          borderSide: BorderSide(color: MColor.kLine.normal),
+        ),
+      ),
+    );
+  }
+}
