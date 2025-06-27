a2ff9eaf6209377643921f2867c8b0794fa6b7c0
son7571 <sssk1755@gmail.com>
2025-06-27 17:10:24 +0900

    [feat] ui - 동행 상세 (#11)
    동행 상세 페이지 화면 구현

--- lib/ui/pages/holder/user_match/detail_page/user_match_detail_page.dart
+++ lib/ui/pages/holder/user_match/detail_page/user_match_detail_page.dart
@@ -1,4 +1,9 @@
+import 'package:ballkkaye_frontend/_core/style/m_text.dart';
+import 'package:ballkkaye_frontend/ui/pages/holder/user_match/list_page/user_match_list_page.dart';
 import 'package:flutter/material.dart';
+
+import 'widgets/user_match_detail_action_sheet.dart';
+import 'widgets/user_match_detail_body.dart';
 
 class UserMatchDetailPage extends StatelessWidget {
   const UserMatchDetailPage({super.key});
@@ -6,11 +11,35 @@ class UserMatchDetailPage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Center(
-        child: Container(
-          child: Text("동행 상세보기 페이지"),
-        ),
+      appBar: _appbar(context),
+      body: UserMatchDetailBody(),
+      floatingActionButton: Padding(
+        padding: const EdgeInsets.only(bottom: 210),
+        child: FloatingActionButton(onPressed: () {
+          Navigator.push(
+            context,
+            MaterialPageRoute(builder: (_) => UserMatchListPage()),
+          );
+        }),
       ),
+    );
+  }
+
+  PreferredSizeWidget _appbar(BuildContext context) {
+    return AppBar(
+      title: MText.h1("동행"),
+      centerTitle: true,
+      leading: IconButton(
+        icon: Icon(Icons.arrow_back),
+        onPressed: () {
+          Navigator.pop(context);
+        },
+      ),
+      actions: [
+        UserMatchDetailActionSheet(),
+        SizedBox(width: 16),
+      ],
+      elevation: 0,
     );
   }
 }
---
+++ lib/ui/pages/holder/user_match/detail_page/widgets/user_match_detail_actionbar.dart
@@ -0,0 +1,47 @@
+import 'package:ballkkaye_frontend/_core/style/m_color.dart';
+import 'package:ballkkaye_frontend/_core/style/m_icon.dart';
+import 'package:ballkkaye_frontend/_core/style/m_text.dart';
+import 'package:flutter/material.dart';
+
+class UserMatchDetailActionBar extends StatelessWidget {
+  const UserMatchDetailActionBar({
+    super.key,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return Row(
+      mainAxisAlignment: MainAxisAlignment.spaceBetween,
+      crossAxisAlignment: CrossAxisAlignment.center,
+      children: [
+        MIcon.page.userMatch.like,
+        Spacer(),
+        Row(
+          mainAxisSize: MainAxisSize.min,
+          crossAxisAlignment: CrossAxisAlignment.end, //
+          children: [
+            Padding(
+              padding: EdgeInsets.only(bottom: 2), //
+              child: Text(
+                '현재 2/6명 참여중',
+                style: TextStyle(fontSize: 12, fontWeight: FontWeight.w400, color: MColor.kLabel.alternative),
+              ),
+            ),
+            SizedBox(width: 12.5),
+            ElevatedButton(
+              onPressed: () {},
+              style: ElevatedButton.styleFrom(
+                backgroundColor: MColor.kPrimary.strong,
+                shape: RoundedRectangleBorder(
+                  borderRadius: BorderRadius.circular(4),
+                ),
+                padding: EdgeInsets.symmetric(horizontal: 21),
+              ),
+              child: MText.button1('채팅하기', color: MColor.kLabel.white),
+            ),
+          ],
+        ),
+      ],
+    );
+  }
+}
