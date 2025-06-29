09e1020f1074cb83dacbdc78797f6bfc1aef7e76
son7571 <sssk1755@gmail.com>
2025-06-30 00:02:38 +0900

    [feat] ui - 동행 작성, 수정 (#13)
    동행 수정 화면 레이아웃 및 UI 구성 완료
    작성 페이지(write_page)는 이동용으로 임시화면으로 구성

---
+++ lib/ui/pages/holder/chat_room/detail_page/chat_room_detail_page.dart
@@ -0,0 +1,14 @@
+import 'package:flutter/material.dart';
+
+class ChatRoomDetailPage extends StatelessWidget {
+  const ChatRoomDetailPage({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Center(
+      child: Container(
+        child: Text("채팅방 페이지"),
+      ),
+    );
+  }
+}
---
+++ lib/ui/pages/holder/user_match/update_page/user_match_update_page.dart
@@ -0,0 +1,35 @@
+import 'package:ballkkaye_frontend/_core/style/m_color.dart';
+import 'package:ballkkaye_frontend/_core/style/m_text.dart';
+import 'package:ballkkaye_frontend/ui/pages/holder/chat_room/detail_page/chat_room_detail_page.dart';
+import 'package:ballkkaye_frontend/ui/pages/holder/chat_room/list_page/chat_room_list_page.dart';
+import 'package:ballkkaye_frontend/ui/pages/holder/user_match/update_page/widgets/user_match_update_body.dart';
+import 'package:flutter/material.dart';
+
+class UserMatchUpdatePage extends StatelessWidget {
+  const UserMatchUpdatePage({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: _appbar(context),
+      body: UserMatchUpdateBody(),
+        floatingActionButton: FloatingActionButton(onPressed: () {
+            Navigator.push(
+              context,
+              MaterialPageRoute(builder: (_) => ChatRoomListPage()),
+            );
+          }),
+    );
+  }
+
+  AppBar _appbar(BuildContext context) {
+    return AppBar(
+      title: MText.h1('동행 작성', color: MColor.kLabel.normal),
+      leading: IconButton(
+        icon: const Icon(Icons.arrow_back),
+        onPressed: () => Navigator.pop(context),
+      ),
+      centerTitle: true,
+    );
+  }
+}
