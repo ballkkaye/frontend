37fb5d4bf75617ae0f2854fe4572548875a087f3
jh0804 <wngml0708@gmail.com>
2025-06-30 04:23:33 +0900

    승패 태그 추가중

--- lib/ui/pages/holder/visit_record/detail_page/widgets/detail_body.dart
+++ lib/ui/pages/holder/visit_record/detail_page/widgets/detail_body.dart
@@ -1,3 +1,4 @@
+import 'package:ballkkaye_frontend/_core/style/m_color.dart';
 import 'package:ballkkaye_frontend/ui/pages/holder/visit_record/detail_page/widgets/detail_content.dart';
 import 'package:ballkkaye_frontend/ui/pages/holder/visit_record/detail_page/widgets/detail_game.dart';
 import 'package:ballkkaye_frontend/ui/pages/holder/visit_record/detail_page/widgets/detail_img.dart';
@@ -10,22 +11,47 @@ class DetailBody extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
+    String result = 'win';
+
     return SingleChildScrollView(
       padding: EdgeInsets.symmetric(horizontal: 16, vertical: 22),
-      child: Card(
-        elevation: 5,
-        child: Padding(
-          padding: EdgeInsets.all(14),
-          child: Column(
-            children: [
-              DetailGame(),
-              SizedBox(height: 19),
-              DetailImg(),
-              SizedBox(height: 19),
-              DetailContent(),
-            ],
+      child: Column(
+        children: [
+          Container(
+            height: 30,
+            width: 60,
+            margin: EdgeInsetsGeometry.directional(start: 250, end: 16),
+            decoration: BoxDecoration(
+              color: result == 'win'
+                  ? MColor.kPrimary.strong
+                  : MColor.kLabel.disable,
+              borderRadius: BorderRadius.only(
+                topLeft: Radius.circular(8),
+                topRight: Radius.circular(8),
+              ),
+            ),
+            child: Center(
+                child: Text(
+              '승',
+              style: TextStyle(fontSize: 14, color: Colors.white),
+            )),
           ),
-        ),
+          Card(
+            elevation: 5,
+            child: Padding(
+              padding: EdgeInsets.all(14),
+              child: Column(
+                children: [
+                  DetailGame(),
+                  SizedBox(height: 19),
+                  DetailImg(),
+                  SizedBox(height: 19),
+                  DetailContent(),
+                ],
+              ),
+            ),
+          ),
+        ],
       ),
     );
   }
