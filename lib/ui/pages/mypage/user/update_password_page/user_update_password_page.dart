5a38b8b46e5211ccb824ade0c91423340e33a412
son7571 <sssk1755@gmail.com>
2025-07-03 09:14:05 +0900

    [feat] ui - 회원 정보 수정 (#37)
    작업 개요
    마이페이지 > 회원정보 수정(프로필) 화면 UI를 설계하고 구성했습니다.
    
    주요 변경사항
    • user_update_body.dart 프로필 사진, 닉네임, 응원팀 입력 필드 UI 구현
    • UserUpdateOutlinedinputField, UserUpdateSelectBtn 위젯 재사용
    • update_page/widgets 디렉토리 정리 및 컨벤션 적용
    • 추후 비밀번호 변경 페이지로 이동하는 버튼 라우팅 준비

---
+++ lib/ui/pages/mypage/user/update_password_page/user_update_password_page.dart
@@ -0,0 +1,13 @@
+import 'package:flutter/material.dart';
+
+
+class UserUpdatePasswordPage extends StatelessWidget {
+  const UserUpdatePasswordPage({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Center(child: Text("UserUpdatePasswordPage")),
+    );
+  }
+}
