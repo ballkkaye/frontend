96625d2d043a482a3c7e42eab7e6088c8cfb20f4
jh0804 <wngml0708@gmail.com>
2025-07-04 20:11:28 +0900

    동행글 상세 ui 수정

--- lib/ui/pages/holder/user_match/detail_page/widgets/user_match_detail_body.dart
+++ lib/ui/pages/holder/user_match/detail_page/widgets/user_match_detail_body.dart
@@ -16,49 +16,30 @@ class UserMatchDetailBody extends StatelessWidget {
     return Padding(
       padding: const EdgeInsets.symmetric(horizontal: 16),
       child: SingleChildScrollView(
-        child: ConstrainedBox(
-          constraints: BoxConstraints(
-            minHeight: MediaQuery.of(context).size.height -
-                kToolbarHeight -
-                MediaQuery.of(context).padding.top -
-                MediaQuery.of(context).padding.bottom,
-          ),
-          child: IntrinsicHeight(
-            child: Column(
-              mainAxisAlignment: MainAxisAlignment.spaceBetween,
-              children: [
-                Column(
-                  crossAxisAlignment: CrossAxisAlignment.start,
-                  children: [
-                    // 제목 + 시간
-                    SizedBox(height: 22),
-                    UserMatchDetailProfile(),
-                    SizedBox(height: 32),
-                    MText.normal4_6('직관같이갑시다', color: MColor.kLabel.normal),
-                    SizedBox(height: 13.5),
-                    MText.normal6_4(
-                      '같이 응원을 열심히 하는 사람들만\n아직 표를 구매하지 않았으니 같이 티켓팅 해봅시다!\n아직 표를 구매하지 않았으니 같이 티켓팅 해봅시다!\n아직 표를 구매하지 않았으니 같이 티켓팅 해봅시다!\n아직 표를 구매하지 않았으니 같이 티켓팅 해봅시다!\n아직 표를 구매하지 않았으니 같이 티켓팅 해봅시다!\n아직 표를 구매하지 않았으니 같이 티켓팅 해봅시다!\n아직 표를 구매하지 않았으니 같이 티켓팅 해봅시다!\n아직 표를 구매하지 않았으니 같이 티켓팅 해봅시다!\n아직 표를 구매하지 않았으니 같이 티켓팅 해봅시다!\n아직 표를 구매하지 않았으니 같이 티켓팅 해봅시다!\n아직 표를 구매하지 않았으니 같이 티켓팅 해봅시다!\n아직 표를 구매하지 않았으니 같이 티켓팅 해봅시다!\n아직 표를 구매하지 않았으니 같이 티켓팅 해봅시다!\n아직 표를 구매하지 않았으니 같이 티켓팅 해봅시다!\n아직 표를 구매하지 않았으니 같이 티켓팅 해봅시다!\n아직 표를 구매하지 않았으니 같이 티켓팅 해봅시다!\n아직 표를 구매하지 않았으니 같이 티켓팅 해봅시다!\n아직 표를 구매하지 않았으니 같이 티켓팅 해봅시다!\n아직 표를 구매하지 않았으니 같이 티켓팅 해봅시다!\n아직 표를 구매하지 않았으니 같이 티켓팅 해봅시다!\n아직 표를 구매하지 않았으니 같이 티켓팅 해봅시다!\n아직 표를 구매하지 않았으니 같이 티켓팅 해봅시다!',
-                      color: MColor.kLabel.normal,
-                    ),
-                  ],
-                ),
-                SizedBox(height: 8),
-                Column(
-                  children: [
-                    // 경기 정보 카드
-                    UserMatchDetailMatchCard(),
-                    SizedBox(height: 9),
-                    // 태그들
-                    UserMatchDetailTagGroup(),
-                    SizedBox(height: 23),
-                    // 참여 인원 + 버튼
-                    UserMatchDetailActionBar(),
-                    SizedBox(height: 12),
-                  ],
-                ),
-              ],
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
+            // 제목 + 시간
+            SizedBox(height: 22),
+            UserMatchDetailProfile(),
+            SizedBox(height: 22),
+            MText.normal4_6('직관같이갑시다', color: MColor.kLabel.normal),
+            SizedBox(height: 13.5),
+            MText.normal6_4(
+              '같이 응원을 열심히 하는 사람들만\n아직 표를 구매하지 않았으니 같이 티켓팅 해봅시다!\n아직 표를 구매하지 않았으니 같이 티켓팅 해봅시다!\n아직 표를 구매하지 않았으니 같이 티켓팅 해봅시다!\n아직 표를 구매하지 않았으니 같이 티켓팅 해봅시다!\n아직 표를 구매하지 않았으니 같이 티켓팅 해봅시다!\n아직 표를 구매하지 않았으니 같이 티켓팅 해봅시다!\n아직 표를 구매하지 않았으니 같이 티켓팅 해봅시다!\n아직 표를 구매하지 않았으니 같이 티켓팅 해봅시다!\n아직 표를 구매하지 않았으니 같이 티켓팅 해봅시다!\n아직 표를 구매하지 않았으니 같이 티켓팅 해봅시다!\n아직 표를 구매하지 않았으니 같이 티켓팅 해봅시다!\n아직 표를 구매하지 않았으니 같이 티켓팅 해봅시다!\n아직 표를 구매하지 않았으니 같이 티켓팅 해봅시다!\n아직 표를 구매하지 않았으니 같이 티켓팅 해봅시다!\n아직 표를 구매하지 않았으니 같이 티켓팅 해봅시다!\n아직 표를 구매하지 않았으니 같이 티켓팅 해봅시다!\n아직 표를 구매하지 않았으니 같이 티켓팅 해봅시다!\n아직 표를 구매하지 않았으니 같이 티켓팅 해봅시다!\n아직 표를 구매하지 않았으니 같이 티켓팅 해봅시다!\n아직 표를 구매하지 않았으니 같이 티켓팅 해봅시다!\n아직 표를 구매하지 않았으니 같이 티켓팅 해봅시다!\n아직 표를 구매하지 않았으니 같이 티켓팅 해봅시다!',
+              color: MColor.kLabel.normal,
             ),
-          ),
+            SizedBox(height: 22),
+            // 경기 정보 카드
+            UserMatchDetailMatchCard(),
+            SizedBox(height: 9),
+            // 태그들
+            UserMatchDetailTagGroup(),
+            SizedBox(height: 22),
+            // 참여 인원 + 버튼
+            UserMatchDetailActionBar(),
+            SizedBox(height: 12),
+          ],
         ),
       ),
     );
