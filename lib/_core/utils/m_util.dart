0db687f7ae3227e6437c52345e7b066a075e8b5a
Seo Hoejeong <139827011+clubnerdy@users.noreply.github.com>
2025-07-11 14:10:23 +0900

    [feat] 직관 기록 - 작성 전 날짜 및 경기 선택 (#80)
    경기목록 조회 메서드/경기가 있는 날짜 조회 메서드 구현
    경기목록에서 게임을 조회할 때, 게임 이름이 없어 필드에서 뽑아서 출력함
    경기 선택 했을 때, 해당 경기의 gameId만 저장됨 ->다음페이지로 아이디만 들고가서 매핑할 수 있도록

--- lib/_core/utils/m_util.dart
+++ lib/_core/utils/m_util.dart
@@ -14,3 +14,14 @@ String formatDateToStr(DateTime dateTime) {
 
   return "$year-$month-$day";
 }
+
+/// `DateTime`을 'yyyy-MM-dd' 형식의 문자열로 변환
+String formatDateToYMD(DateTime date) {
+  return DateFormat('yyyy-MM-dd').format(date);
+}
+
+/// dash로 구분된 날짜 형식 dot로 구분된 날짜 형식으로 변환
+String formatToDotDate(String dashDate) {
+  // "2025-07-10" → "2025.07.10"
+  return dashDate.replaceAll("-", ".");
+}
--- lib/data/repository/visit_record_repository.dart
+++ lib/data/repository/visit_record_repository.dart
@@ -90,4 +90,86 @@ class VisitRecordRepository {
     };
     return responseBody;
   }
+
+  // select 경기 리스트 조회
+  Future<Map<String, dynamic>> getGameList(String date) async {
+    // Response response = await dio.get("${/s/api/games}");
+    final responseBody = {
+      "status": 100,
+      "msg": "성공",
+      "body": {
+        "games": [
+          {
+            "gameDate": "2025.07.10",
+            "items": [
+              {
+                "gameId": 425,
+                "homeTeamFullName": "NC 다이노스",
+                "homeTeamShortName": "NC",
+                "homeTeamScore": 3,
+                "awayTeamFullName": "롯데 자이언츠",
+                "awayTeamShortName": "롯데",
+                "awayTeamScore": 5,
+                "stadiumFullName": "창원 NC파크",
+                "stadiumShortName": "창원",
+                "gameDate": "2025.07.02"
+              },
+              {
+                "gameId": 426,
+                "homeTeamFullName": "KT 위즈",
+                "homeTeamShortName": "KT",
+                "homeTeamScore": 5,
+                "awayTeamFullName": "LG 트윈스",
+                "awayTeamShortName": "LG",
+                "awayTeamScore": 5,
+                "stadiumFullName": "수원 KT위즈파크",
+                "stadiumShortName": "수원",
+                "gameDate": "2025.07.02"
+              },
+              {
+                "gameId": 427,
+                "homeTeamFullName": "키움 히어로즈",
+                "homeTeamShortName": "키움",
+                "homeTeamScore": 2,
+                "awayTeamFullName": "KIA 타이거즈",
+                "awayTeamShortName": "KIA",
+                "awayTeamScore": 0,
+                "stadiumFullName": "고척스카이돔",
+                "stadiumShortName": "고척",
+                "gameDate": "2025.07.02"
+              }
+            ]
+          }
+        ],
+        "selectedDate": "2025-07-10"
+      }
+    };
+    return responseBody;
+  }
+
+  // select 경기가 있는 날짜 조회
+  Future<Map<String, dynamic>> getHasGameDay(String date) async {
+    // Response response = await dio.get("${/s/api/games/dates}");
+    final responseBody = {
+      "status": 200,
+      "msg": "성공",
+      "body": [
+        {
+          "year": "2025",
+          "monthDTO": [
+            {
+              "month": "07",
+              "day": [
+                {"day": "02", "isHaveGame": true},
+                {"day": "03", "isHaveGame": true},
+                {"day": "04", "isHaveGame": true},
+                {"day": "05", "isHaveGame": true}
+              ]
+            }
+          ]
+        }
+      ]
+    };
+    return responseBody;
+  }
 }
