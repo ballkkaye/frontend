59212f746f92c6254517add95e73607f5e56fe1b
son <sssk1755@gmail.com>
2025-07-13 14:40:53 +0900

    승리예측 구현

--- lib/ui/pages/holder/home/widget/home_prediction.dart
+++ lib/ui/pages/holder/home/widget/home_prediction.dart
@@ -1,15 +1,22 @@
 import 'package:ballkkaye_frontend/ui/pages/holder/game_center/prediction_page/prediction_page.dart';
+import 'package:ballkkaye_frontend/ui/pages/holder/home/home_vm.dart';
 import 'package:ballkkaye_frontend/ui/pages/holder/home/widget/home_heading.dart';
 import 'package:ballkkaye_frontend/ui/pages/holder/home/widget/home_prediction_slider.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
 
-class HomePrediction extends StatelessWidget {
+class HomePrediction extends ConsumerWidget {
   const HomePrediction({
     super.key,
   });
 
   @override
-  Widget build(BuildContext context) {
+  Widget build(BuildContext context,WidgetRef ref) {
+    final homeModel = ref.watch(homeProvider);
+    if (homeModel == null) {
+      return Center(child: CircularProgressIndicator());
+    } else {
+      final predictions  = homeModel.predictionModel.games;
     return Column(
       children: [
         HomeHeading(
@@ -17,8 +24,9 @@ class HomePrediction extends StatelessWidget {
           uri: PredictionPage(),
         ),
         SizedBox(height: 14),
-        HomePredictionSlider(),
+        HomePredictionSlider(games : predictions),
       ],
     );
+    }
   }
 }
