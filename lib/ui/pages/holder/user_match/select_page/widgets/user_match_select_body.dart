9f3b1b65593d4ffdf3c10afbc8b12d89bc0ab91e
JH <127887466+jh0804@users.noreply.github.com>
2025-07-14 15:44:44 +0900

    [fix] 동행 - 작성 전 날짜 및 경기 선택 vm 수정  (#94)
    동행글 작성 전 날짜와 경기를 선택하는 vm이 나눠져있었고 그로 인해 selectedDate가 상태로 제대로 관리되지 않는 문제 발생
    -> 날짜와 경기, 선택된 날짜를 한번에 상태로 관리하도록 통합 vm을 생성 해서 그 안에서 관리되도록 함

--- lib/ui/pages/holder/user_match/select_page/widgets/user_match_select_body.dart
+++ lib/ui/pages/holder/user_match/select_page/widgets/user_match_select_body.dart
@@ -1,11 +1,10 @@
 import 'package:ballkkaye_frontend/_core/style/m_text.dart';
 import 'package:ballkkaye_frontend/_core/utils/m_util.dart';
-import 'package:ballkkaye_frontend/ui/pages/holder/user_match/select_page/user_match_select_date_vm.dart';
+import 'package:ballkkaye_frontend/ui/pages/holder/user_match/select_page/user_match_select_vm.dart';
 import 'package:ballkkaye_frontend/ui/pages/holder/user_match/select_page/widgets/user_match_select_dropdown_btn.dart';
 import 'package:ballkkaye_frontend/ui/widgets/m_date_select_btn.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
-import 'package:logger/logger.dart';
 
 class UserMatchSelectBody extends ConsumerStatefulWidget {
   const UserMatchSelectBody({super.key});
@@ -17,58 +16,38 @@ class UserMatchSelectBody extends ConsumerStatefulWidget {
 class _UserMatchSelectBodyState extends ConsumerState<UserMatchSelectBody> {
   String selectedDate = formatDateToStr(today);
 
-  List<String> games = [
-    '두산 베어스 vs 롯데 자이언츠 (사직)',
-    'SSG 랜더스 vs LG 트윈스 (잠실)',
-    '삼성 라이온즈 vs 기아 타이거즈 (광주)',
-    'KT 위즈 vs 한화 이글스 (대전)',
-    'NC 다이노스 vs 키움 히어로즈 (고척)'
-  ];
-
   @override
   Widget build(BuildContext context) {
-    UserMatchSelectDateModel? dateModel = ref.watch(userMatchSelectDateProvider);
+    UserMatchSelectVM vm = ref.read(userMatchSelectProvider(selectedDate).notifier);
 
-    if (dateModel == null) {
-      return Center(child: CircularProgressIndicator());
-    } else {
-      return Padding(
-        padding: EdgeInsets.symmetric(vertical: 22, horizontal: 16),
-        child: Form(
-          child: Column(
-            crossAxisAlignment: CrossAxisAlignment.start,
-            children: [
-              MText.h3_6("날짜 선택"),
-              SizedBox(height: 10),
-              // 날짜 선택 버튼
-              MDateSelectBtn(
-                text: selectedDate,
-                minimumDate: today,
-                maximumDate: dateModel.maximumDate,
-                onDateTimeChanged: (value) {
-                  Logger().d(dateModel.isGameDay(value));
-                  if (!dateModel.isGameDay(value)) {
-                    ScaffoldMessenger.of(context).showSnackBar(
-                      SnackBar(content: Text("경기가 없는 날입니다.")),
-                    );
-                    return;
-                  }
-                  setState(() {
-                    selectedDate = formatDateToStr(value);
-                  });
-                  // formatDateToStr(value);
-                  // TODO : value 날짜를 select_game_vm으로 넘겨줘야 한다.
-                },
-              ),
-              SizedBox(height: 16),
-              MText.h3_6("경기 선택"),
-              SizedBox(height: 10),
-              // 경기 선택 버튼
-              UserMatchSelectDropdownBtn(date: selectedDate),
-            ],
-          ),
+    return Padding(
+      padding: EdgeInsets.symmetric(vertical: 22, horizontal: 16),
+      child: Form(
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
+            MText.h3_6("날짜 선택"),
+            SizedBox(height: 10),
+            // 날짜 선택 버튼
+            MDateSelectBtn(
+              text: selectedDate,
+              minimumDate: DateTime(DateTime.now().year, 1, 1),
+              maximumDate: DateTime(DateTime.now().year + 1, 1, 0),
+              onDateTimeChanged: (date) {
+                setState(() {
+                  selectedDate = formatDateToStr(date);
+                });
+                vm.getAvailableDate(formatDateToStr(date));
+              },
+            ),
+            SizedBox(height: 16),
+            MText.h3_6("경기 선택"),
+            SizedBox(height: 10),
+            // 경기 선택 버튼
+            UserMatchSelectDropdownBtn(date: selectedDate),
+          ],
         ),
-      );
-    }
+      ),
+    );
   }
 }
