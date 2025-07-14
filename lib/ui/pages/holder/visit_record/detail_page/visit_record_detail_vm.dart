b43dd63cccc9d8b2c45c68065b01dd01059b8b55
SeoHoejoeng <seohoejeong@gmail.com>
2025-07-15 00:47:25 +0900

    직관기록 삭제

--- lib/ui/pages/holder/visit_record/detail_page/visit_record_detail_vm.dart
+++ lib/ui/pages/holder/visit_record/detail_page/visit_record_detail_vm.dart
@@ -1,15 +1,18 @@
 import 'package:ballkkaye_frontend/data/model/visit_record.dart';
 import 'package:ballkkaye_frontend/data/repository/visit_record_repository.dart';
 import 'package:ballkkaye_frontend/main.dart';
+import 'package:ballkkaye_frontend/ui/pages/holder/visit_record/list_page/visit_record_list_vm.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:logger/logger.dart';
 
-final visitRecordDetailProvider = AutoDisposeNotifierProvider.family<VisitRecordDetailVM, VisitRecordDetailModel?, int>(() {
+final visitRecordDetailProvider = AutoDisposeNotifierProvider.family<
+    VisitRecordDetailVM, VisitRecordDetailModel?, int>(() {
   return VisitRecordDetailVM();
 });
 
-class VisitRecordDetailVM extends AutoDisposeFamilyNotifier<VisitRecordDetailModel?, int> {
+class VisitRecordDetailVM
+    extends AutoDisposeFamilyNotifier<VisitRecordDetailModel?, int> {
   final mContext = navigatorKey.currentContext!;
 
   @override
@@ -24,7 +27,8 @@ class VisitRecordDetailVM extends AutoDisposeFamilyNotifier<VisitRecordDetailMod
   }
 
   Future<void> init(int visitRecordId) async {
-    Map<String, dynamic> body = await VisitRecordRepository().getOne(visitRecordId);
+    Map<String, dynamic> body =
+        await VisitRecordRepository().getOne(visitRecordId);
     if (body["status"] != 200) {
       ScaffoldMessenger.of(mContext!).showSnackBar(
         SnackBar(content: Text("게시글 상세보기 실패 : ${body["msg"]}")),
@@ -34,6 +38,18 @@ class VisitRecordDetailVM extends AutoDisposeFamilyNotifier<VisitRecordDetailMod
     state = VisitRecordDetailModel.fromMap(body["body"]);
     Logger().d(state);
   }
+
+  Future<void> deleteOne(int visitRecordId, WidgetRef ref) async {
+    final body = await VisitRecordRepository().deleteOne(visitRecordId);
+    if (body["status"] != 200) {
+      ScaffoldMessenger.of(navigatorKey.currentContext!).showSnackBar(
+        SnackBar(content: Text("직관 기록 삭제 실패: ${body["errorMessage"]}")),
+      );
+      return;
+    }
+    ref.read(visitRecordListProvider.notifier).notifyDeleteOne(visitRecordId);
+    Navigator.pop(navigatorKey.currentContext!);
+  }
 }
 
 class VisitRecordDetailModel {
@@ -41,7 +57,8 @@ class VisitRecordDetailModel {
 
   VisitRecordDetailModel(this.visitRecord);
 
-  VisitRecordDetailModel.fromMap(Map<String, dynamic> data) : visitRecord = VisitRecord.fromMap(data);
+  VisitRecordDetailModel.fromMap(Map<String, dynamic> data)
+      : visitRecord = VisitRecord.fromMap(data);
 
   VisitRecordDetailModel copyWith({VisitRecord? visitRecord}) {
     return VisitRecordDetailModel(visitRecord ?? this.visitRecord);
